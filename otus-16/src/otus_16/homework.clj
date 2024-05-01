66a6bce169d327f430bad281ae30bcd3e8f4682a
Vadzimz <56486120+Vadzimz@users.noreply.github.com>
2024-05-01 12:54:32 +0300

    Update homework.clj

--- otus-16/src/otus_16/homework.clj
+++ otus-16/src/otus_16/homework.clj
@@ -2,28 +2,28 @@
   (:require [clojure.string :as str]
             [clojure.java.io :as io]))
 
+(def reg #".+\] \".*?/(.*?) .*?\" \d+ (\d+) \"(.*?)\".+")
+
 (defn parse-line [line]
-  (let [reg #".+\] \".*?/(.*?) .*?\" \d+ (\d+) \"(.*?)\".+"
-        [_ url bytes referer] (re-matches reg line)]
-    {:url url 
-     :bytes (if (str/blank? bytes) 0 (Integer/parseInt bytes)) 
+  (let [[_ url bytes referer] (re-matches reg line)]
+    {:url url
+     :bytes (if (str/blank? bytes) 0 (Integer/parseInt bytes))
      :referer referer}))
 
 ; при вводе без ключей и с ключом url-requested читаем sum-bytes, c ключом referer-requested - count-url
 (defn read-log [log-file & {:keys [url-requested referer-requested]}]
   (with-open [rdr (io/reader log-file)]
-    (loop [rem-log (line-seq rdr)
-           acc {:count-url 0 :sum-bytes 0}]
-      (if-not (seq rem-log) acc
-              (recur (drop 2 rem-log)
-                     (let [{:keys [url bytes referer]} (parse-line (first rem-log))] 
-                       (if (or (= url url-requested) 
-                               (= referer referer-requested) 
-                               (every? nil? [url-requested referer-requested])) 
-                         (-> acc 
-                             (update :count-url inc)
-                             (update :sum-bytes #(+ % bytes)))
-                         acc)))))))
+    (->> (take-nth 2 (line-seq rdr))
+         (map parse-line)
+         (reduce (fn [acc {:keys [url bytes referer]}]
+                   (cond-> acc 
+                     (or (= url url-requested) 
+                         (= referer referer-requested) 
+                         (every? nil? [url-requested referer-requested])) 
+                     ((fn [x] (-> x 
+                                 (update :count-url inc) 
+                                 (update :sum-bytes #(+ % bytes)))))))
+                 {:count-url 0 :sum-bytes 0}))))
 
 (defn read-logs [log-files & {:keys [url-requested referer-requested]}]
   (->> (pmap #(read-log % :url-requested url-requested
